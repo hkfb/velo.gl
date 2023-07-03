4f082881d15dd4ff18c5d78f93377f2dd04b67db
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-03 21:49:51 +0200

    Created default story.

--- gpx-layer.stories.tsx
+++ gpx-layer.stories.tsx
@@ -7,9 +7,34 @@ export default {
   title: "GPX Layer",
 };
 
-export function GPXLayer() {
-  const gpxFile = "Morning_Ride.gpx";
+const gpxFile = "Morning_Ride.gpx";
 
+const initialViewState = {
+  longitude: 10.8,
+  latitude: 59.85,
+  zoom: 15,
+};
+
+export function GPXLayerDefault() {
+  const layer = useMemo(
+    () =>
+      new GpxLayer({
+        id: "gpx-layer",
+        data: gpxFile,
+      }),
+    [gpxFile]
+  );
+
+  return (
+    <DeckGL layers={[layer]} initialViewState={initialViewState} controller>
+      <div style={{ position: "absolute", top: "10px", left: "10px" }}>
+        GPX Example
+      </div>
+    </DeckGL>
+  );
+}
+
+export function GPXLayerLineStyle() {
   const layer = useMemo(
     () =>
       new GpxLayer({
@@ -20,12 +45,6 @@ export function GPXLayer() {
       }),
     [gpxFile]
   );
-
-  const initialViewState = {
-    longitude: 10.8,
-    latitude: 59.79,
-    zoom: 12,
-  };
 
   return (
     <DeckGL layers={[layer]} initialViewState={initialViewState} controller>
