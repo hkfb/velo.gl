6c9409caa73b51f140f0bec6c3cbc435f180cbf5
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-04 18:45:00 +0200

    Added WMS layer example.

--- gpx-layer.stories.tsx
+++ gpx-layer.stories.tsx
@@ -1,4 +1,5 @@
 import { DeckGL } from "@deck.gl/react/typed";
+import { _WMSLayer } from "@deck.gl/geo-layers/typed";
 import { useMemo } from "react";
 import * as React from "react";
 import { GpxLayer } from "./gpx-layer";
@@ -7,12 +8,12 @@ export default {
   title: "GPX Layer",
 };
 
-const gpxFile = "Morning_Ride.gpx";
+const gpxFile = "Jotunheimen_rundt.gpx";
 
 const initialViewState = {
-  longitude: 10.8,
-  latitude: 59.85,
-  zoom: 15,
+  longitude: 8.3,
+  latitude: 61.4,
+  zoom: 8,
 };
 
 export function GPXLayerDefault() {
@@ -48,6 +49,35 @@ export function GPXLayerLineStyle() {
 
   return (
     <DeckGL layers={[layer]} initialViewState={initialViewState} controller>
+      <div style={{ position: "absolute", top: "10px", left: "10px" }}>
+        GPX Example
+      </div>
+    </DeckGL>
+  );
+}
+
+export function GpxWms() {
+  const gpxLayer = useMemo(
+    () =>
+      new GpxLayer({
+        id: "gpx-layer",
+        data: gpxFile,
+        lineWidthMinPixels: 5,
+        getLineColor: [0, 0, 0],
+      }),
+    [gpxFile]
+  );
+
+  const wmsLayer = new _WMSLayer({
+    data: "https://ows.terrestris.de/osm/service",
+    serviceType: "wms",
+    layers: ["OSM-WMS"],
+  });
+
+  const layers = [gpxLayer, wmsLayer];
+
+  return (
+    <DeckGL layers={layers} initialViewState={initialViewState} controller>
       <div style={{ position: "absolute", top: "10px", left: "10px" }}>
         GPX Example
       </div>
