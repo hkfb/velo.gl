787790b6522b7d059a204881dd1f5272fd02a36f
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-07 22:48:29 +0200

    Fixed lint condition.

--- src/street-layer.ts
+++ src/street-layer.ts
@@ -1,10 +1,9 @@
-import { _WMSLayer, WMSLayerProps } from "@deck.gl/geo-layers/typed";
+import { _WMSLayer } from "@deck.gl/geo-layers/typed";
 
 export class StreetLayer extends _WMSLayer {
   constructor() {
     const props = {
       data: "https://ows.terrestris.de/osm/service",
-      //serviceType: "wms",
       layers: ["OSM-WMS"],
     };
     super(props);
