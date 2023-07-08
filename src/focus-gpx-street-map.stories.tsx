9385fc5947663886687e5447afdb808e2849cb1d
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-08 22:15:14 +0200

    Added GPX street map with auto-focus.

---
+++ src/focus-gpx-street-map.stories.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { FocusGpxStreetMap } from "./focus-gpx-street-map";
+
+export default {
+  title: "Focus GPX Street Map",
+};
+
+export function FocusGpxStreetMapDefault() {
+  return <FocusGpxStreetMap></FocusGpxStreetMap>;
+}
+
+export function FocusGpxStreetMapSelect({ gpxUrl }: { gpxUrl: string }) {
+  return <FocusGpxStreetMap gpx={gpxUrl}></FocusGpxStreetMap>;
+}
+
+FocusGpxStreetMapSelect.args = {
+  gpxUrl: "Jotunheimen_rundt.gpx",
+};
+
+FocusGpxStreetMapSelect.argTypes = {
+  gpxUrl: {
+    control: "select",
+    options: [
+      "Jotunheimen_rundt.gpx",
+      "https://cdn.cyclingstage.com/images/tour-de-france/2023/stage-7-parcours.gpx",
+    ],
+  },
+};
---
+++ src/focus-gpx-street-map.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FocusGpxMap } from "./focus-gpx-map";
+import { StreetLayer } from "./street-layer";
+
+export type FocusGpxStreetMapProps = {
+  children?: React.ReactNode;
+  gpx?: string;
+};
+
+export function FocusGpxStreetMap({ children, gpx }: FocusGpxStreetMapProps) {
+  return (
+    <FocusGpxMap gpx={gpx} auxLayers={[new StreetLayer()]}>
+      {children}
+    </FocusGpxMap>
+  );
+}
