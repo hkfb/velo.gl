bd472a30b2779545c3473b85122be806412847a0
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-08 22:26:59 +0200

    Added export for FocusGpxStreetMap.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { GpxMap } from "./gpx-map";
 export { GpxLayer } from "./gpx-layer";
 export { GpxStreetMap } from "./gpx-street-map";
 export { StreetLayer } from "./street-layer";
+export { FocusGpxStreetMap } from "./focus-gpx-street-map";
