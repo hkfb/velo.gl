980adc8ea8bdba08c1f129c5bbac18dd0f005c53
Hvard Bjerke <hkfbjerke@gmail.com>
2023-07-09 13:22:29 +0200

    Render tracks with outlines.

--- src/gpx-layer.ts
+++ src/gpx-layer.ts
@@ -1,13 +1,41 @@
-import { GeoJsonLayer, GeoJsonLayerProps } from "@deck.gl/layers/typed";
+import {
+  GeoJsonLayer,
+  GeoJsonLayerProps,
+  PathLayer,
+} from "@deck.gl/layers/typed";
+import { Color } from "@deck.gl/core/typed";
 import { GPXLoader } from "@loaders.gl/kml";
+import * as _ from "lodash";
 
 export class GpxLayer extends GeoJsonLayer {
   constructor(props: GeoJsonLayerProps) {
     props.loaders = [GPXLoader];
     super(props);
   }
+
+  renderLayers() {
+    const defaultLayers = super.renderLayers();
+    const lineLayers = defaultLayers[1] as PathLayer[];
+    if (!_.isArray(lineLayers) || lineLayers.length < 2) {
+      return defaultLayers;
+    }
+    const pathLayer = lineLayers[1];
+    if (pathLayer.constructor !== PathLayer) {
+      return defaultLayers;
+    }
+    const outlineLayer = pathLayer.clone({
+      id: "outline",
+      widthMinPixels: this.props.lineWidthMinPixels + 1,
+      getColor: [0, 0, 0],
+    });
+    return [outlineLayer, ...defaultLayers];
+  }
 }
 
-const defaultProps = { ...GeoJsonLayer.defaultProps, lineWidthMinPixels: 2 };
+const defaultProps = {
+  ...GeoJsonLayer.defaultProps,
+  lineWidthMinPixels: 3,
+  getLineColor: [255, 255, 0] as Color,
+};
 
 GpxLayer.defaultProps = defaultProps;
