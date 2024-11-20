31db3aa254d70bb05ad0b362f1ca3d13eb498e5b
Hvard Bjerke <hkfbjerke@gmail.com>
2024-11-20 19:00:35 +0100

    fix: prevent scrollbar in GPX Maptiler component

--- src/components/gpx-maptiler.tsx
+++ src/components/gpx-maptiler.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import { Map } from "react-map-gl/maplibre";
 import { FocusGpxMap, FocusGpxMapProps } from "./focus-gpx-map";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 export function GpxMaptiler(args: FocusGpxMapProps) {
-    const [apiKey] = React.useState(import.meta.env.VITE_MAPTILER_API_KEY);
+    const apiKey = import.meta.env.VITE_MAPTILER_API_KEY;
     const mapStyle = `https://api.maptiler.com/maps/streets-v2/style.json?key=${apiKey}`;
     return (
         <FocusGpxMap {...args}>
             <Map
                 mapStyle={mapStyle}
-                style={{ position: "relative", zIndex: "-100" }}
+                style={{ position: "absolute", zIndex: "-100" }}
+                attributionControl={false}
             />
             {args.children}
         </FocusGpxMap>
