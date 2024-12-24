9741455a15f90046c12edf1a9e774dd7cad5600c
Hvard Bjerke <hkfbjerke@gmail.com>
2024-12-24 10:50:48 +0100

    fix: sanitize missing coordinates

--- src/layers/activity-layer/activity-layer.ts
+++ src/layers/activity-layer/activity-layer.ts
@@ -7,6 +7,7 @@ import {
 } from "../face-profile-layer/face-profile-layer";
 import { simplify } from "@turf/simplify";
 import { coordEach } from "@turf/meta";
+import _ from "lodash";
 
 export type ActivityLayerProps<DataT = unknown> = FaceProfileLayerProps<DataT>;
 
@@ -32,13 +33,15 @@ function dataTransform(collection: FeatureCollection) {
     const simplified = simplify(feature, { tolerance: 0.0003 });
     const geometry = simplified.geometry as LineString;
 
-    // Validate coordinates.
+    // Sanitize coordinates.
     coordEach(geometry, (coord: Position) => {
-        if (coord.length !== 3) {
-            throw new Error(
-                "Some coordinates do not have exactly three values.",
-            );
+        if (coord.length === 3 && !_.isUndefined(coord[2])) {
+            return coord;
         }
+
+        console.info(`Encountered non 3D coordinates: ${coord}`);
+
+        return [coord[0], coord[1], 0];
     });
 
     const coordinates = geometry.coordinates;
