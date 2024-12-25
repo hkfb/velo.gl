7f1d5d66158c86ee3aae6320d64d181478df3629
Hvard Bjerke <hkfbjerke@gmail.com>
2024-12-25 15:28:38 +0100

    test: updated failed story snapshot

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -17,7 +17,7 @@ const getBrowserName = (page: Page) => {
 const getPollInterval = (page: Page) => {
     const browserName = getBrowserName(page);
     if (browserName === "webkit") {
-        return 25000;
+        return 30000;
     }
     return 15000;
 }
